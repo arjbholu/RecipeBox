303c56b172bff2c6843acdecc70db37c1c1e8719
arjbholu@gmail.com <arjbholu@gmail.com>
2018-02-19 20:02:05 +0530

    Added Actions

---
+++ src/actions/index.js
@@ -0,0 +1,22 @@
+export const add = () => {
+    return {
+        type: 'ADD',
+        addEdit: 1,
+        add: 1
+    }
+}
+
+export const edit = () => {
+    return {
+        type: 'EDIT',
+        addEdit: 1,
+        edit: 1
+    }
+}
+
+export const save = (data) => {
+    return {
+        type: 'SAVE',
+        data
+    }
+}
